10e3442806ec2952b84a9d7dd8272cb64a9becba
Dato Simó <dato@net.com.org.es>
2019-01-04 02:55:14 -0300

    Advent of code 2015/10.

---
+++ 2015/src/day10.rs
@@ -0,0 +1,43 @@
+// https://adventofcode.com/2015/day/10
+
+use itertools::Itertools;
+
+fn iter(string: &str, times: i32) -> String {
+    let mut digits = string.to_owned();
+
+    // Used with Itertools::coalesce() to count contiguous runs of characters.
+    let contiguous = |(n, x), (m, y)| {
+        if x == y {
+            Ok((n + m, x))
+        } else {
+            Err(((n, x), (m, y)))
+        }
+    };
+
+    for _ in 0..times {
+        let mut s = String::new();
+        for (n, c) in digits.chars().map(|c| (1, c)).coalesce(contiguous) {
+            match n {
+                0...9 => {
+                    // Cast to char to avoid an allocation.
+                    let d = (n as u8) + ('0' as u8);
+                    s.push(d as char);
+                }
+                _ => {
+                    s.push_str(&n.to_string());
+                }
+            }
+            s.push(c);
+        }
+        digits = s;
+    }
+    digits
+}
+
+pub fn day10(filename: Option<&str>) -> (String, String) {
+    let input = super::readline(filename.unwrap_or("input/10"));
+    let one = iter(&input, 40);
+    let two = iter(&input, 50);
+
+    (one.len().to_string(), two.len().to_string())
+}
--- 2015/src/main.rs
+++ 2015/src/main.rs
@@ -2,6 +2,7 @@
 extern crate text_io;
 
 mod day1;
+mod day10;
 mod day2;
 mod day3;
 mod day4;
@@ -22,7 +23,7 @@ type DayFunc = fn(Option<&str>) -> (String, String);
 #[rustfmt::skip]
 static FUNCS: &'static [DayFunc] = &[
     day1::day1, day2::day2, day3::day3, day4::day4, day5::day5, day6::day6,
-    day7::day7, day8::day8, day9::day9,
+    day7::day7, day8::day8, day9::day9, day10::day10,
 ];
 
 fn bufread(filename: &str) -> BufReader<File> {
@@ -115,5 +116,12 @@ mod tests {
         let (a, b) = super::day9::day9(None);
         assert_eq!("141", a);
         assert_eq!("736", b);
+    }
+
+    #[test]
+    fn day10() {
+        let (a, b) = super::day10::day10(None);
+        assert_eq!("329356", a);
+        assert_eq!("4666278", b);
     }
 }
