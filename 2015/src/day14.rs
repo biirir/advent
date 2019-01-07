a9277034c3f35972b42d49d7c14a4eb997a65497
Dato Simó <dato@net.com.org.es>
2019-01-07 09:25:26 -0300

    Advent of code 2015/14.

---
+++ 2015/src/day14.rs
@@ -0,0 +1,97 @@
+// https://adventofcode.com/2015/day/14
+
+use std::cmp::{max, min};
+use std::io::BufRead;
+
+struct Reindeer {
+    _name: String,
+    speed: u32,
+    time_on: u32,
+    time_off: u32,
+}
+
+struct ScoringReindeer {
+    r: Reindeer,
+    kms: u32,
+    ticks: u32,
+    points: u32,
+}
+
+impl Reindeer {
+    fn travelled_distance(&self, secs: u32) -> u32 {
+        let cycle = self.time_on + self.time_off;
+        let cycles = secs / cycle;
+        let remaining = secs % cycle;
+
+        self.speed * (cycles * self.time_on + min(remaining, self.time_on))
+    }
+}
+
+impl ScoringReindeer {
+    /// Adds a second, updating kilometers travelled so far.
+    fn tick(&mut self) {
+        self.ticks += 1;
+        let cycle = self.r.time_on + self.r.time_off;
+        let current = self.ticks % cycle;
+
+        if current > 0 && current <= self.r.time_on {
+            self.kms += self.r.speed;
+        }
+    }
+
+    /// Adds one point for this reindeer.
+    fn point(&mut self) {
+        self.points += 1;
+    }
+}
+
+pub fn day14(filename: Option<&str>) -> (String, String) {
+    // Part 1: just arithmetic on each Reindeer.
+    let vec: Vec<_> = super::bufread(filename.unwrap_or("input/14")).lines()
+        .map(|line| {
+            let line = line.unwrap();
+            let (_name, speed, time_on, time_off);
+            scan!(line.bytes() => "{} can fly {} km/s for {} seconds, but then must rest for {} seconds.",
+                  _name, speed, time_on, time_off);
+            Reindeer {_name, speed, time_on, time_off}
+        })
+        .collect();
+
+    let kms = vec
+        .iter()
+        .map(|r| r.travelled_distance(2503))
+        .max()
+        .unwrap();
+
+    // Part 2: need to keep track of each step.
+    let mut vec: Vec<_> = vec
+        .into_iter()
+        .map(|r| ScoringReindeer {
+            r,
+            kms: 0,
+            ticks: 0,
+            points: 0,
+        })
+        .collect();
+
+    for _ in 0..2503 {
+        let mut best = 0;
+
+        // Advance.
+        for r in vec.iter_mut() {
+            r.tick();
+            best = max(best, r.kms);
+        }
+
+        // Score.
+        for r in vec.iter_mut() {
+            if r.kms == best {
+                r.point();
+            }
+        }
+    }
+
+    let points = vec.iter().map(|r| r.points).max().unwrap();
+
+    (kms.to_string(), points.to_string())
+}
--- 2015/src/main.rs
+++ 2015/src/main.rs
@@ -5,6 +5,7 @@ mod day1;
 mod day10;
 mod day11;
 mod day13;
+mod day14;
 mod day2;
 mod day3;
 mod day4;
@@ -26,7 +27,7 @@ type DayFunc = fn(Option<&str>) -> (String, String);
 static FUNCS: &'static [DayFunc] = &[
     day1::day1, day2::day2, day3::day3, day4::day4, day5::day5, day6::day6,
     day7::day7, day8::day8, day9::day9, day10::day10, day11::day11,
-    day13::day13,
+    day13::day13, day14::day14,
 ];
 
 fn bufread(filename: &str) -> BufReader<File> {
@@ -140,5 +141,12 @@ mod tests {
         let (a, b) = super::day13::day13(None);
         assert_eq!("664", a);
         assert_eq!("640", b);
+    }
+
+    #[test]
+    fn day14() {
+        let (a, b) = super::day14::day14(None);
+        assert_eq!("2640", a);
+        assert_eq!("1102", b);
     }
 }
