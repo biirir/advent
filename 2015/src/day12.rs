3ebd58afc9c2baeb2d90ec47eaa3fd981662caa6
Dato Simó <dato@net.com.org.es>
2019-01-09 18:03:00 -0300

    Advent of code 2015/12.

---
+++ 2015/src/day12.rs
@@ -0,0 +1,35 @@
+// https://adventofcode.com/2015/day/12
+
+use serde_json::Value;
+
+type RejectFilter = fn(v: &Value) -> bool;
+
+fn json_sum(v: &Value, reject: RejectFilter) -> i64 {
+    let json_sum2 = |v| json_sum(v, reject);
+
+    if reject(v) {
+        return 0;
+    }
+
+    match v {
+        Value::Number(n) => n.as_i64().unwrap(),
+        Value::Array(vec) => vec.iter().map(json_sum2).sum(),
+        Value::Object(map) => map.values().map(json_sum2).sum(),
+        _ => 0,
+    }
+}
+
+fn has_red_val(v: &Value) -> bool {
+    v.as_object().map_or(false, |map| {
+        map.values()
+            .any(|e| e.as_str().map_or(false, |s| s == "red"))
+    })
+}
+
+pub fn day12(filename: Option<&str>) -> (String, String) {
+    let v: Value = serde_json::from_reader(super::bufread(filename.unwrap_or("input/12"))).unwrap();
+    (
+        json_sum(&v, |_| false).to_string(),
+        json_sum(&v, has_red_val).to_string(),
+    )
+}
--- 2015/src/main.rs
+++ 2015/src/main.rs
@@ -4,6 +4,7 @@ extern crate text_io;
 mod day1;
 mod day10;
 mod day11;
+mod day12;
 mod day13;
 mod day14;
 mod day15;
@@ -27,7 +28,7 @@ type DayFunc = fn(Option<&str>) -> (String, String);
 #[rustfmt::skip]
 static FUNCS: &'static [DayFunc] = &[
     day1::day1, day2::day2, day3::day3, day4::day4, day5::day5, day6::day6,
-    day7::day7, day8::day8, day9::day9, day10::day10, day11::day11,
+    day7::day7, day8::day8, day9::day9, day10::day10, day11::day11, day12::day12,
     day13::day13, day14::day14, day15::day15,
 ];
 
@@ -135,6 +136,13 @@ mod tests {
         let (a, b) = super::day11::day11(None);
         assert_eq!("hxbxxyzz", a);
         assert_eq!("hxcaabcc", b);
+    }
+
+    #[test]
+    fn day12() {
+        let (a, b) = super::day12::day12(None);
+        assert_eq!("111754", a);
+        assert_eq!("65402", b);
     }
 
     #[test]
