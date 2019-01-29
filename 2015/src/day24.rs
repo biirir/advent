f528d94a67f89d191d031f52e46f3d8172d1ea1e
Dato Simó <dato@net.com.org.es>
2019-01-29 04:13:19 -0300

    Advent of code 2015/24.

---
+++ 2015/src/day24.rs
@@ -0,0 +1,55 @@
+// https://adventofcode.com/2015/day/24
+
+use std::cmp::min;
+use std::io::BufRead;
+
+pub fn day24(filename: Option<&str>) -> (String, String) {
+    let mut weights = super::bufread(filename.unwrap_or("input/24"))
+        .lines()
+        .map(|l| l.unwrap().parse::<u64>().unwrap())
+        .collect::<Vec<_>>();
+
+    let weight = weights.iter().sum::<u64>();
+    assert_eq!(weight % 3, 0);
+    assert_eq!(weight % 4, 0);
+
+    weights.sort_unstable(); // Input is already sorted, but just in case.
+    weights.reverse();
+
+    let a = fewest_entanglement(&weights, 0, 1, std::u64::MAX, weight / 3);
+    let b = fewest_entanglement(&weights, 0, 1, std::u64::MAX, weight / 4);
+
+    (a.to_string(), b.to_string())
+}
+
+fn fewest_entanglement(weights: &[u64], idx: usize, cur: u64, mut best: u64, target: u64) -> u64 {
+    // This function only searches for the lowest entanglement, so it's wrong
+    // on two counts:
+    //
+    //   - this lowest entanglement could come from a grouping that doesn't allow for
+    //     the other two groups to divide evenly into equal weights. (XXX Is this true?)
+    //
+    //   - a group with higher entanglement could have fewer packages (as in the example
+    //     on the web), and it should take precedence.
+    //
+    // But, it's 4am here. ¯\_(ツ)_/¯
+
+    if target == 0 {
+        return min(cur, best);
+    }
+
+    if idx >= weights.len() || cur > best {
+        return best;
+    }
+
+    let w = weights[idx];
+
+    if w <= target {
+        best = fewest_entanglement(weights, idx + 1, cur * w, best, target - w);
+    }
+
+    min(
+        best,
+        fewest_entanglement(weights, idx + 1, cur, best, target),
+    )
+}
--- 2015/src/main.rs
+++ 2015/src/main.rs
@@ -16,6 +16,7 @@ mod day2;
 mod day20;
 mod day21;
 mod day23;
+mod day24;
 mod day3;
 mod day4;
 mod day5;
@@ -39,7 +40,7 @@ static FUNCS: &'static [DayFunc] = &[
     day1::day1, day2::day2, day3::day3, day4::day4, day5::day5, day6::day6,
     day7::day7, day8::day8, day9::day9, day10::day10, day11::day11, day12::day12,
     day13::day13, day14::day14, day15::day15, day16::day16, day17::day17, day18::day18,
-    day19::day19, day20::day20, day21::day21, day23::day23,
+    day19::day19, day20::day20, day21::day21, day23::day23, day24::day24,
 ];
 
 fn bufread(filename: &str) -> BufReader<File> {
@@ -232,5 +233,12 @@ mod tests {
         let (a, b) = super::day23::day23(None);
         assert_eq!("255", a);
         assert_eq!("334", b);
+    }
+
+    #[test]
+    fn day24() {
+        let (a, b) = super::day24::day24(None);
+        assert_eq!("10439961859", a);
+        assert_eq!("72050269", b);
     }
 }
